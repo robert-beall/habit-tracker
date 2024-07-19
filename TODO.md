2b7270177d465081af77a4ca860f7e1ed739801e
rbeall@smcm.edu <>
2024-07-19 10:01:04 -0400

    Pass env vars to spring and skip tess on build

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # TODO 
 - [x] Add hibernate support for mysql to backend
-- [ ] Code cleanup
+- [x] Code cleanup
 - [ ] Add basic authentication and user to database
 - [ ] Add a nextjs frontend with typescript
 - [ ] Dockerize frontend
--- src/backend/src/test/java/com/rjb/hobby_tracker/HobbyTrackerApplicationTests.java
+++ src/backend/src/test/java/com/rjb/hobby_tracker/HobbyTrackerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.rjb.hobby_tracker;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HobbyTrackerApplicationTests {
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext);
 	}
 
 }
